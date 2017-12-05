16bf0b5899da830bed94f9f0a25d67135488bbfd
mint <mintchipleaf@gmail.com>
2017-12-04 21:55:46 -0800

    Added word separation to sentences

--- PhraseCount.py
+++ PhraseCount.py
@@ -1,5 +1,9 @@
-#Regex
+#Import Regex
 import re
+
+#Settings
+min_phrase = 4
+max_phrase = 10
 
 #Get list of all sentences in document
 sentences = []
@@ -7,6 +11,13 @@ with open ("input.txt") as document:
     for line in document:
 
         #Add contained sentences to list (Currently assumes no abbreviations, acronyms, websites, decimals etc.) (Also not great at newlines.)
-        sentences.extend(re.split(r"\.|\?|\!", line))
+        sentences.extend(re.split(r"\. |\?|\!", line))
 
-        print(sentences)
+        #print(sentences) #Debug
+
+#Build dictionary of phrases
+phrases = {}
+for sentence in sentences:
+    words = re.split('\W+', sentence)
+
+    print(words) #Debug
